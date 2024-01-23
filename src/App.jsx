e9ce1627e5960f18994f5e6baeb4e8d49bf72e41
Paul Sullivan <paul8sullivan@gmail.com>
2024-01-22 23:55:28 -0500

    added about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // src/App.js
 import React from 'react';
 import Navbar from './components/navbar'; // Adjust the path accordingly
-import Main from './components/main/main'
+import Main from './components/main/main';
+import About from './components/about/about';
 // Other imports and component code...
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <div>
       <Navbar /> 
       <Main/>
+      <About/>
     </div>
   );
 };
---
+++ src/components/about/about.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './about.css';
+
+
+const About = () => {
+  return (
+    <section id='about'>
+<span className="aboutTitle">About Me </span>
+<span className="aboutDesc">I am a skilled junior developer... </span>
+<div className="aboutCards">
+    <div className="aboutCard">
+        <img src="" alt="" className='aboutImg'/>
+        <h2></h2>
+        <p></p>
+    </div>
+    <div className="aboutCard">
+        <img src="" alt="" className='aboutImg'/>
+        <h2></h2>
+        <p></p>
+    </div>
+    <div className="aboutCard">
+        <img src="" alt="" className='aboutImg'/>
+        <h2></h2>
+        <p></p>
+    </div>
+</div>
+    </section>
+  )
+}
+
+export default About
