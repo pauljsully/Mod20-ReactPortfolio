5de32558367ce636ab105855edf9cb6a500959a9
Paul Sullivan <141800906+pauljsully@users.noreply.github.com>
2024-01-25 03:44:54 -0500

    Update portfolio.jsx

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../css/portfolio.css';
+import './portfolio.css';
 import GitHub from '../../assets/github.png';
 import Deployed from '../../assets/deployed.png';
 import PatientPlus from '../../assets/PatientPlus.png';
@@ -10,57 +10,62 @@ import Question from '../../assets/question.png';
 const Portfolio = () => {
   const projects = [
     {
-      title: 'Weather App',
+      title: 'Daily Weather App',
       imageSrc: Weather,
+      githubLink: 'https://github.com/pauljsully/Mod6-DailyWeather.git',
+    },
+    {
+      title: 'Diet Ninja',
+      imageSrc: DietNinja,
       githubLink: 'https://github.com/yourusername/project1',
       deployLink: 'https://yourdeployedlinkforproject1.com',
     },
     {
-        title: 'Diet Ninja',
-        imageSrc: DietNinja,
-        githubLink: 'https://github.com/yourusername/project1',
-        deployLink: 'https://yourdeployedlinkforproject1.com',
+      title: 'Patient +',
+      imageSrc: PatientPlus,
+      githubLink: 'https://github.com/pauljsully/Project2-patientPlus',
+      deployLink: 'https://patientplusportal-ec99a88081f0.herokuapp.com/',
     },
     {
-        title: 'Patient +',
-        imageSrc: PatientPlus,
-        githubLink: 'https://github.com/pauljsully/Project2-patientPlus',
-        deployLink: 'https://patientplusportal-ec99a88081f0.herokuapp.com/',
+      title: 'Coming Soon',
+      imageSrc: Question,
+      githubLink: 'https://github.com/yourusername/project1',
+      deployLink: 'https://yourdeployedlinkforproject1.com',
     },
-    {
-        title: 'Coming Soon',
-        imageSrc: Question,
-        githubLink: 'https://github.com/yourusername/project1',
-        deployLink: 'https://yourdeployedlinkforproject1.com',
-    },
-
   ];
 
   return (
     <section id='apps'>
       <h2 className="portfolioTitle">My Portfolio</h2>
 
-      {projects.map((project, index) => (
-        <div className="project" key={index}>
-          <h3>{project.title}</h3>
+      <div className="projectsContainer">
+        {projects.map((project, index) => (
+          <div className="project" key={index}>
+            <div className="projectDetails">
+              <h3>{project.title}</h3>
 
-            <div>
+              <div className='projectImg'>
                 <img src={project.imageSrc} alt={project.title} />
-            </div>
+              </div>
 
-          <div className="links">
-            <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
-              <img src={GitHub} alt="GitHub" />
-            </a>
-            <a href={project.deployLink} target="_blank" rel="noopener noreferrer">
-              <img src={Deployed} alt="Deployed" />
-            </a>
+              <div className="links">
+                <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                  <img src={GitHub} alt="GitHub" />
+                </a>
+                {project.deployLink && (
+                  <a href={project.deployLink} target="_blank" rel="noopener noreferrer">
+                    <img src={Deployed} alt="Deployed" />
+                  </a>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 };
 
 export default Portfolio;
+
 
